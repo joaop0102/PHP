312e08a7b8ac20d6a2226790c9b8ce64430f41ad
joaop0102 <101288165+joaop0102@users.noreply.github.com>
2023-02-13 16:17:34 -0300

    enviando

---
+++ aula1/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php 
+        $var1 = 1;
+        $varX = 9.50;
+        $varY = 9.50;
+        $var2 =$varX + $varY;
+
+        $varSTR = "Atribui String";
+        $varletra = "a";
+        $varbool = true;
+
+        echo "Exemplo de uma aplicação em PHP";
+        print "<p>";
+
+        echo "Conteudo de var1: ";
+        echo $var1;
+        print "<p>";
+
+        echo "Conteudo de varX: ";
+        echo $varX;
+        print "<p>";
+
+        echo "Conteudo de varY: ";
+        echo $varY;
+        print "<p>";
+
+        echo "Conteudo de var2: ";
+        echo $var2;
+        print "<p>";
+
+        echo "Conteudo de varSTR: ";
+        echo $varSTR;
+        print "<p>";
+
+        echo "Conteudo de varletra: ";
+
+        echo $varletra;
+        print "<p>";
+        echo "Conteudo de varbool: ";
+        echo $varbool;
+    
+    ?>
+  </body>
+</html>
